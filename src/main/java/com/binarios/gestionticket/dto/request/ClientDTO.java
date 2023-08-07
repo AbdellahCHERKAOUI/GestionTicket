570ba4032ac0aabd6078b73c587df7f497ab847d
baxynl <abdelghanielaoud@gmail.com>
2023-08-07 16:24:59 +0200

    tech,admin,client controllers

---
+++ src/main/java/com/binarios/gestionticket/dto/request/ClientDTO.java
@@ -0,0 +1,82 @@
+package com.binarios.gestionticket.dto.request;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+    private String fullName;
+    private String password;
+    private String role;
+    private String email;
+    private String phoneNumber;
+    private LocalDate birthDate;
+    private Long group;
+
+    public ClientDTO() {
+    }
+
+    public ClientDTO(String fullName, String password, String role, String email, String phoneNumber, LocalDate birthDate, Long group) {
+        this.fullName = fullName;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.group = group;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Long getGroup() {
+        return group;
+    }
+
+    public void setGroup(Long group) {
+        this.group = group;
+    }
+}
---
+++ src/main/java/com/binarios/gestionticket/dto/response/ClientResponseDTO.java
@@ -0,0 +1,102 @@
+package com.binarios.gestionticket.dto.response;
+
+import java.time.LocalDate;
+
+public class ClientResponseDTO {
+    private Long id;
+    private String username;
+    private String fullName;
+    private String password;
+    private String role;
+    private String email;
+    private String phoneNumber;
+    private LocalDate birthDate;
+    private Long group;
+
+    public ClientResponseDTO() {
+    }
+
+    public ClientResponseDTO(Long id, String username, String fullName, String password, String role, String email, String phoneNumber, LocalDate birthDate, Long group) {
+        this.id = id;
+        this.username = username;
+        this.fullName = fullName;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+        this.group = group;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Long getGroup() {
+        return group;
+    }
+
+    public void setGroup(Long group) {
+        this.group = group;
+    }
+}
